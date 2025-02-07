e03bc1b1fa8274173eb7842f2174148db01ec96c
Arnaught <rayquaza01@outlook.com>
2025-02-07 15:53:02 -0500

    Update script to work on gnome and kde

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 # sets the wallpaper to the bing wallpaper of the day
-# requires jq to be installed
-# only works on gnome
+# requires curl and jq
+# works with gnome (gsettings) and kde (plasma-apply-wallpaperimage)
 
+PICTURES_DIR="$HOME/Pictures"
+if command -v xdg-user-dir &>/dev/null; then
+    PICTURES_DIR=$(xdg-user-dir PICTURES)
+fi
 # Location for wallpapers to be saved
-WALLPAPER_LOCATION="$(xdg-user-dir PICTURES)/bing-wallpaper"
+WALLPAPER_LOCATION="$PICTURES_DIR/bing-wallpaper"
+
+if ! [ -d "$WALLPAPER_LOCATION" ]; then
+    mkdir -p "$WALLPAPER_LOCATION"
+fi
+
 # Timestamp for file name
 TIMESTAMP=$(date +"%Y%m%d")
 # File location
@@ -20,7 +29,17 @@ if [ ! -f "$FILENAME" ]; then
 
     curl "https://bing.com$IMAGE_URL_BASE" -o "$FILENAME"
 
-    gsettings set org.gnome.desktop.background picture-uri "file://$FILENAME"
+    # apply wallpaper on GNOME
+    # apply as both dark and light wallpaper
+    if command -v gsettings &>/dev/null; then
+        gsettings set org.gnome.desktop.background picture-uri "file://$FILENAME"
+        gsettings set org.gnome.desktop.background picture-uri-dark "file://$FILENAME"
+    fi
+
+    # apply wallpaper on KDE
+    if command -v plasma-apply-wallpaperimage &>/dev/null; then
+        plasma-apply-wallpaperimage "$FILENAME"
+    fi
 fi
 
 if [ "$KEEP_FOR" -ge 0 ]; then
