97a2aa9b833629fe56841a88be8b97f05ca9b0b2
Arnaught <rayquaza01@outlook.com>
2025-02-07 19:32:35 -0500

    Ensure dirs exist

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 SYSTEMD_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/systemd/user"
 BIN_DIR="$HOME/.local/bin"
 
+mkdir -p "$SYSTEMD_DIR"
+mkdir -p "$BIN_DIR"
+
 cp -v bing-wallpaper "$BIN_DIR"
 cp -v systemd/bingwallpaper.service "$SYSTEMD_DIR"
 cp -v systemd/bingwallpaper.timer "$SYSTEMD_DIR"
@@ -11,6 +14,6 @@ systemctl --user daemon-reload
 echo "Enable the service with:"
 echo "$ systemctl --user enable --now bingwallpaper.timer"
 echo ""
-echo "Disable the service with"
+echo "Disable the service with:"
 echo "$ systemctl --user disable bingwallpaper.timer"
 echo "$ systemctl --user stop bingwallpaper.timer"
